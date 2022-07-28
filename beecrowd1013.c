709771ead314b2fd5094dbe4269bf9784e69f2b7
TawabC223134 <109740218+TawabC223134@users.noreply.github.com>
2022-07-28 21:20:27 +0600

    Add files via upload

---
+++ beecrowd1013.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a,b,c,d,e;
+    scanf("%d%d%d",&a,&b,&c);
+    d=(a+b+abs(a-b))/2;
+    e=(c+d+abs(c-d))/2;
+    printf("%d eh o maior\n",e);
+    return 0;
+}

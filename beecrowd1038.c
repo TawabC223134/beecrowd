6c13b98754109149c1698fdd25c6f4d6c974559f
TawabC223134 <109740218+TawabC223134@users.noreply.github.com>
2022-07-30 22:59:55 +0600

    Add files via upload

---
+++ beecrowd1038.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    double c;
+    scanf("%d%d",&a,&b);
+    if(a==1)
+    {
+        c=b*4.00;
+        printf("Total: R$ %.2lf\n",c);
+    }
+    else if(a==2)
+    {
+        c=b*4.50;
+        printf("Total: R$ %.2lf\n",c);
+    }
+    else if(a==3)
+    {
+        c=b*5.00;
+        printf("Total: R$ %.2lf\n",c);
+    }
+    else if(a==4)
+    {
+        c=b*2.00;
+        printf("Total: R$ %.2lf\n",c);
+    }
+    else if(a==5)
+    {
+        c=b*1.50;
+        printf("Total: R$ %.2lf\n",c);
+    }
+}
+

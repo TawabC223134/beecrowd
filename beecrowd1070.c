3e9b6bcf5009c345f49d668c688357c0fcb16f58
TawabC223134 <109740218+TawabC223134@users.noreply.github.com>
2022-08-15 11:03:28 +0600

    Add files via upload

---
+++ beecrowd1070.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+
+    scanf("%d",&n);
+    i=n;
+
+    while(i<(n+12))
+    {
+        if(i%2!=0)
+        {
+            printf("%d\n",i);
+        }
+        i++;
+    }
+    return 0;
+
+}
+
